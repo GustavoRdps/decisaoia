645f1772f3129891ef55f6a3d51430865a572001
GustavoRdps <gustavosilva.bexidas@gmail.com>
2025-09-16 12:06:39 -0400

    correção

--- src/modules/scenario/scenario.service.ts
+++ src/modules/scenario/scenario.service.ts
@@ -2,28 +2,38 @@ import { Injectable } from '@nestjs/common';
 import { CreateScenarioDto } from './dto/create-scenario.dto';
 import { UpdateScenarioDto } from './dto/update-scenario.dto';
 import { CreateScenarioUseCase } from './use-cases';
+import { ListScenarioRepositoryUseCase } from './use-cases/list-scenario.use-case';
+import { FindOneScenarioUseCase } from './use-cases/find-one-scenario.use-case';
+import { UpdateScenarioUseCase } from './use-cases/update-scenario.use-case';
+import { RemoveScenarioUseCase } from './use-cases/remove-scenario.use-case';
 
 @Injectable()
 export class ScenarioService {
-  constructor(private readonly createScenarioUseCase: CreateScenarioUseCase) {}
+  constructor(
+    private readonly createScenarioUseCase: CreateScenarioUseCase,
+    private readonly listAllUseCase: ListScenarioRepositoryUseCase,
+    private readonly findOneUseCase: FindOneScenarioUseCase,
+    private readonly updateUseCase: UpdateScenarioUseCase,
+    private readonly removeUseCase: RemoveScenarioUseCase
+  ) {}
 
   create(createScenarioDto: CreateScenarioDto) {
     return this.createScenarioUseCase.execute(createScenarioDto);
   }
 
   findAll() {
-    return `This action returns all scenario`;
+    return this.listAllUseCase.execute();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} scenario`;
+  findOne(id: string) {
+    return this.findOneUseCase.execute(id);
   }
 
-  update(id: number, updateScenarioDto: UpdateScenarioDto) {
-    return `This action updates a #${id} scenario`;
+  update(id: string, updateScenarioDto: UpdateScenarioDto) {
+    return this.updateUseCase.execute(id, updateScenarioDto);
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} scenario`;
+  remove(id: string) {
+    return this.removeUseCase.execute(id);
   }
 }
